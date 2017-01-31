dacff34d0554f127d6f873c530c80a073cf393d0
Jelmer Draaijer <info@jelmert.nl>
2017-01-31 09:59:44 +0100

    ModelLoop in background threat and progressbar in dialog

--- model-gui.py
+++ model-gui.py
@@ -1,7 +1,9 @@
 import os
+import threading
 import tkinter as tk
 import xml.etree.cElementTree as ET
-from _datetime import datetime
+from datetime import datetime
+from queue import Queue
 from tkinter import filedialog
 from tkinter import ttk
 
@@ -39,15 +41,23 @@ class MainApplication(tk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
+
         self.parent = parent
+        self.queue = Queue()
+        self.interrupt = False
+        self.tid = None
 
         # variable used
         self.input_file = tk.StringVar()
         self.output_dir = tk.StringVar()
         self.model = tk.StringVar()
         self.iterations = tk.StringVar()
+        self.counter = tk.IntVar()
 
+        # load settings from xml file
         self.load_settings()
+
+        # layout
         row = self.row()
         self.label("Select input file", row=row)
         self.input_btn = ttk.Button(parent, text="Input file", command=self.input)
@@ -73,11 +83,20 @@ class MainApplication(tk.Frame):
         r2 = tk.Radiobutton(parent, text="Random Rate", variable=self.model, value="random")
         r2.grid(row=self.row(), column=1, sticky=tk.W)
 
-        self.run = tk.Button(parent, text="Run", command=self.run)
-        self.run.grid(row=self.row(), column=1, sticky=tk.E)
+        self.run_btn = tk.Button(parent, text="Run", command=self.run_model)
+        self.run_btn.grid(row=self.row(), column=1, sticky=tk.E)
 
-        self.progress_bar = ttk.Progressbar(self.parent, orient="horizontal", length=100, mode="determinate")
-        self.progress_bar.grid(row=self.row(), column=0, columnspan=2, sticky=tk.E+tk.W)
+        self.progress_dialog = None
+
+    def progress_bar(self, maximum):
+
+        self.counter.set(0)
+        self.progress_dialog = tk.Toplevel(self.parent)
+        tk.Label(self.progress_dialog, text="Download Bar").pack(side=tk.TOP)
+
+        ttk.Progressbar(self.progress_dialog, orient="horizontal", length=400, mode="determinate", variable=self.counter, maximum=maximum).pack(side=tk.TOP)
+
+        center(self.progress_dialog)
 
     def input(self):
         dialog = tk.filedialog.askopenfile()
@@ -92,16 +111,6 @@ class MainApplication(tk.Frame):
             os.makedirs(dir)
 
         self.output_dir.set(dir)
-
-    def run(self):
-        print("run")
-        print(self.E1.get())
-        print(self.input_file)
-        print(self.output_dir)
-        print(self.serialize())
-
-        self.save_settings()
-        self.run_model()
 
     def model_type(self):
         print(self.model.get())
@@ -119,7 +128,7 @@ class MainApplication(tk.Frame):
         return ret
 
     def label(self, text, row, column=0):
-        tk.Label(self.parent, text=text).grid(row=row, column=column, sticky=tk.W)
+        tk.Label(self.parent, text=text).grid(row=row, column=column, sticky=tk.W, padx=20, pady=20)
 
     def serialize(self):
         return ET.tostring(self.to_xml())
@@ -141,8 +150,10 @@ class MainApplication(tk.Frame):
 
         element = ET.Element("model-settings")
 
+        ignore = ["counter"]
+
         for key, value in self.__dict__.items():
-            if isinstance(value, tk.Variable):
+            if isinstance(value, tk.Variable) and key not in ignore:
                 child = ET.Element(key)
                 child.text = value.get()
                 element.append(child)
@@ -150,6 +161,15 @@ class MainApplication(tk.Frame):
         return element
 
     def run_model(self):
+        self.save_settings()
+
+        t = threading.Thread(target=self.run)
+        t.start()
+        self.periodic_call()
+
+        self.progress_bar(int(self.iterations.get()))
+
+    def run(self):
 
         if self.model.get() == "equal":
             from model.equalgain import EqualGainModel as Model
@@ -157,48 +177,70 @@ class MainApplication(tk.Frame):
             from model.randomrate import RandomRateModel as Model
 
         # The event handlers for logging and writing the results to the disk.
-        eventHandler = Observable()
-        Logger(eventHandler)
-        po = ProgressObserver(eventHandler)
+        event_handler = Observable()
+        Logger(event_handler)
 
-        po.progressbar = self.progress_bar
-        po.step_size = 100 / int(self.iterations.get())
+        start_time = datetime.now()
 
-        startTime = datetime.now()
-
-        eventHandler.notify(Observable.LOG, message="Start calculation at {0}".format(startTime))
+        event_handler.notify(Observable.LOG, message="Start calculation at {0}".format(start_time))
 
         model = Model()
 
         from model.helpers import csvParser
-        csvParser = csvParser.Parser(model)
+        csv_parser = csvParser.Parser(model)
 
         data_set_name = os.path.join(self.output_dir.get(), self.input_file.get().split("/")[-1].split(".")[0])
 
-        model = csvParser.read(self.input_file.get())
+        model = csv_parser.read(self.input_file.get())
 
-        Externalities(eventHandler, model, data_set_name)
-        ExchangesWriter(eventHandler, model, data_set_name)
-        HistoryWriter(eventHandler, model, data_set_name)
-        InitialExchanges(eventHandler)
-        eventHandler.notify(Observable.LOG, message="Parsed file {0}".format(self.input_file.get()))
+        Externalities(event_handler, model, data_set_name)
+        ExchangesWriter(event_handler, model, data_set_name)
+        HistoryWriter(event_handler, model, data_set_name)
+        InitialExchanges(event_handler)
+        event_handler.notify(Observable.LOG, message="Parsed file {0}".format(self.input_file.get()))
 
-        model_loop = ModelLoop(model, eventHandler)
+        model_loop = ModelLoop(model, event_handler)
 
         for iteration_number in range(int(self.iterations.get())):
-            model_loop.loop()
-            self.parent.update_idletasks()
 
-        eventHandler.notify(Observable.CLOSE, model=self.model)
-        eventHandler.notify(Observable.LOG, message="Finished in {0}".format(datetime.now() - startTime))
+            if not self.interrupt:
+                model_loop.loop()
+                self.queue.put(iteration_number)
+            else:
+                print("interrupted")
+                break
+
+        event_handler.notify(Observable.CLOSE, model=self.model)
+        event_handler.notify(Observable.LOG, message="Finished in {0}".format(datetime.now() - start_time))
+
+        self.stop_periodic_call()
+
+    def prog_bar_update(self, value):
+        self.counter.set(value + 1)
+
+    def process_queue(self):
+        while self.queue.qsize():
+            try:
+                value = self.queue.get(0)
+                self.prog_bar_update(value)
+            except Queue.Empty:
+                pass
+
+    def periodic_call(self):
+        self.process_queue()
+        self.tid = self.parent.after(100, self.periodic_call)
+
+    def stop_periodic_call(self):
+        self.parent.after_cancel(self.tid)
+        self.progress_dialog.destroy()
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = MainApplication(root)
+    app = MainApplication(root, width=500, height=500)
 
-    root.minsize(width=500, height=500)
     center(root)
 
     root.title("Model")
+
     root.mainloop()
