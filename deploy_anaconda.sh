e06b0ce6129e16114de75f29410e51665da7818e
Jelmer Draaijer <jelmerdraaijer@gmail.com>
2018-03-05 10:58:30 +0100

    Continuous delivery

---
+++ deploy_anaconda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# https://gist.github.com/yoavram/05a3c04ddcf317a517d5
+set -e
+
+echo "Converting conda package..."
+conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/decide-exchange-model-*.tar.bz2 --output-dir conda-bld/
+
+echo "Deploying to Anaconda.org..."
+anaconda -t $ANACONDA_TOKEN upload conda-bld/**/decide-exchange-model-*.tar.bz2
+
+echo "Successfully deployed to Anaconda.org."
+exit 0
