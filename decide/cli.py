db00fe100ca7a8fa831ec34967ac0789b7b2d84d
Jelmert <jelmerdraaijer@gmail.com>
2019-01-05 17:51:48 +0100

    Bug fixed in the float_range function

--- decide/cli.py
+++ decide/cli.py
@@ -62,12 +62,20 @@ def init_output_directory(model, output_dir, selected_actors=list()):
 
 
 def float_range(start=0.0, stop=1.0, step=0.05):
-    i = start
-    while i < (
-        stop + step / 10
-    ):  # add a 10th step to stop for floating point rounding differences 0.500001 vs 0.499999
-        yield i
-        i += step
+
+    # when the step size is 0, return
+    if step == 0:
+        yield 0
+    else:
+        # prevent locking yourself out
+        if (stop - start) / step > 256:
+            raise RuntimeError('Maximum steps exceeded with step={} (256)'.format(step))
+
+        i = start
+        # add a 10th step to stop for floating point rounding differences 0.500001 vs 0.499999
+        while i < (stop + step / 10):
+                yield i
+                i += step
 
 
 def main():
---
+++ decide/tests/cli_test.py
@@ -0,0 +1,24 @@
+from nose.tools import raises
+
+
+@raises(RuntimeError)
+def test_float_range():
+    from decide.cli import float_range
+
+    xrange = list(float_range(0, 0.20, 0.05))
+
+    assert sum([0.0, 0.05, 0.10, 0.15, 0.20]) == sum(xrange)
+
+    assert 0 == sum(float_range(0, 0, 0))
+
+    values = list(float_range(0.05, 0.1, 0.05))
+
+    assert 0.15 == round(sum(values), 2), sum(values)
+
+    assert 0 == sum(float_range(0, 0, 0.10))
+
+    assert 0 == sum(float_range(0, 1, 0.0))
+
+    assert 0 == sum(float_range(0, 100, 0.00005)), 'Should trigger RuntimeError'
+
+    assert 0 == sum(float_range(0, 1, 0.0))
