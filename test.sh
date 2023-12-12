230c2f38b1894c32d16cb69e1d2f9276397a0ad5
Jelmer Draaijer <info@jelmert.nl>
2023-12-12 15:05:45 +0100

    Update Anaconda publish to use libmamba

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh -O miniconda.sh;
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.10.0-1-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
-conda install 'conda-build==3.22.0'
-conda build .
+$HOME/miniconda/bin/conda install -n base --yes conda-build conda-libmamba-solver conda-verify
+$HOME/miniconda/bin/conda config --set solver libmamba
+$HOME/miniconda/bin/conda create decide
+$HOME/miniconda/bin/conda build --numpy 1.23.4 --python 3.9 .
+$HOME/miniconda/bin/conda env export > environment.yml
